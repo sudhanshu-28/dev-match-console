166bc10fe77c0c0d8f5a7001f35aee98296a824b
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-20 11:35:04 +0530

    User Token validation and Profile fetch API Integrated.

--- src/api-config/endpoints.js
+++ src/api-config/endpoints.js
@@ -1,4 +1,8 @@
 export const BASE_URL = "http://localhost:7777";
 
+// Auth
 export const LOGIN_API = "/auth/login";
 export const LOGOUT_API = "/auth/logout";
+
+// Profile
+export const PROFILE_VIEW_API = "/profile/view";
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,43 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
+import { addUser } from "../utils/userSlice";
+import { BASE_URL, PROFILE_VIEW_API } from "../api-config/endpoints";
+
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUserProfile = async () => {
+    try {
+      const response = await axios.get(BASE_URL + PROFILE_VIEW_API, {
+        withCredentials: true,
+      });
+
+      const res = response?.data;
+      const { success = false, data = null } = res;
+
+      if (success && data) {
+        dispatch(addUser(data));
+      }
+    } catch (error) {
+      if (error?.status === 401) {
+        navigate("/login");
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
