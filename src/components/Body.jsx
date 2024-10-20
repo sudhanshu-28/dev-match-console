7cbe587bd3b4633a1abd9dc8a9278baeb3d0f4c7
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-20 11:54:42 +0530

    Multiple API calls resolved for fetching User Profile details.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import axios from "axios";
-import { useDispatch } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import NavBar from "./NavBar";
 import Footer from "./Footer";
@@ -12,8 +12,14 @@ import { BASE_URL, PROFILE_VIEW_API } from "../api-config/endpoints";
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const fetchUserProfile = async () => {
+  const LOGIN_PATH = "/login";
+  const currentPath = location.pathname;
+
+  const userData = useSelector((store) => store?.user);
+
+  const fetchUserDetails = async () => {
     try {
       const response = await axios.get(BASE_URL + PROFILE_VIEW_API, {
         withCredentials: true,
@@ -35,8 +41,10 @@ const Body = () => {
   };
 
   useEffect(() => {
-    fetchUserProfile();
-  }, []);
+    if (!userData && currentPath !== LOGIN_PATH) {
+      fetchUserDetails();
+    }
+  }, [userData, currentPath]);
 
   return (
     <div className="flex flex-col min-h-screen">
