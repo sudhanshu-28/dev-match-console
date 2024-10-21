d74fbd7ed54a3332b884e03675e329b226e3b80c
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-21 10:52:49 +0530

    Tailwind scrollbar added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 import daisyui from "daisyui";
+import tailwindScrollbar from "tailwind-scrollbar";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [daisyui, tailwindScrollbar({ nocompatible: true })],
   daisyui: {
     themes: ["light", "nord", "lemonade", "dim"],
   },
