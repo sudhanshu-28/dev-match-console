0b37aaf6d8328503beabfd13414f055b6c35f41a
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-21 20:38:51 +0530

    Skeleton resuable component created.

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,4 +1,4 @@
-const Skeleton = () => {
+export const CardSkeleton = () => {
   return (
     <div className="flex w-52 flex-col gap-4">
       <div className="skeleton h-32 w-full"></div>
@@ -9,4 +9,19 @@ const Skeleton = () => {
   );
 };
 
-export default Skeleton;
+export const ProfileSkeleton = () => (
+  <div className="w-full bg-base-100 flex h-24 rounded-xl p-4 gap-5 justify-between">
+    <div className="flex items-center gap-4">
+      <div className="skeleton bg-gray-300 h-16 w-16 shrink-0 rounded-xl"></div>
+      <div className="flex flex-col gap-2">
+        <div className="skeleton h-4 w-32"></div>
+        <div className="skeleton h-4 w-20"></div>
+        <div className="skeleton h-4 w-80"></div>
+      </div>
+    </div>
+    <div className="flex gap-2 items-center">
+      <div className="skeleton bg-gray-300 h-8 w-16 shrink-0 rounded-xl"></div>
+      <div className="skeleton bg-gray-300 h-8 w-16 shrink-0 rounded-xl"></div>
+    </div>
+  </div>
+);
