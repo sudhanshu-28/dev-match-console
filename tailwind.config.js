d8c58a92736e75f5949662431ebb5a12b3d72d61
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-21 17:24:34 +0530

    tailwindcss/line-clamp added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 
 import daisyui from "daisyui";
 import tailwindScrollbar from "tailwind-scrollbar";
+import lineClamp from "@tailwindcss/line-clamp";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui, tailwindScrollbar({ nocompatible: true })],
+  plugins: [daisyui, tailwindScrollbar({ nocompatible: true }), lineClamp],
   daisyui: {
     themes: ["light", "nord", "lemonade", "dim"],
   },
