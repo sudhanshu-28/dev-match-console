94a8c95255cf3c7b60da4d94fe54a69de50e1e4e
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-21 18:00:42 +0530

    Connection Requests page integrated with Accept/Reject API Integration.

--- src/api-config/endpoints.js
+++ src/api-config/endpoints.js
@@ -14,3 +14,5 @@ export const CONNECTIONS_API = "/user/connections";
 
 // Request
 export const REQUEST_RECEIVED_API = "/user/requests/received";
+export const REQUEST_ACCEPT_API = "/request/review/accepted";
+export const REQUEST_REJECT_API = "/request/review/rejected";
--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,7 +1,85 @@
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+
+import { addConnectionRequests } from "../utils/requestSlice";
+
+import {
+  BASE_URL,
+  REQUEST_ACCEPT_API,
+  REQUEST_RECEIVED_API,
+  REQUEST_REJECT_API,
+} from "../api-config/endpoints";
+import { showErrorMessage, showSuccessMessage } from "../utils/notifySlice";
 
 const Requests = () => {
+  const dispatch = useDispatch();
+
   const { data: connectionRequests } = useSelector((store) => store?.request);
+
+  const fetchConnectionRequests = async () => {
+    await axios
+      .get(BASE_URL + REQUEST_RECEIVED_API, { withCredentials: true })
+      .then((res) => {
+        const response = res?.data;
+        const { success = false, data = null } = response;
+        if (success && data) {
+          dispatch(addConnectionRequests(data));
+        }
+      })
+      .catch((err) => {
+        console.error(err?.message || err);
+      });
+  };
+
+  const handleRequestAccept = async (connectionId) => {
+    await axios
+      .post(
+        `${BASE_URL}${REQUEST_ACCEPT_API}/${connectionId}`,
+        {},
+        { withCredentials: true }
+      )
+      .then((res) => {
+        const response = res?.data;
+        const { success = false, data = null, message } = response;
+        if (success && data) {
+          fetchConnectionRequests();
+          dispatch(showSuccessMessage(message));
+        }
+      })
+      .catch((err) => {
+        const response = err?.response?.data;
+        const { success, message } = response;
+        if (!success) {
+          const displayMsg = message || "Failed to accept Connection request.";
+          dispatch(showErrorMessage(displayMsg));
+        }
+      });
+  };
+
+  const handleRequestReject = async (connectionId) => {
+    await axios
+      .post(
+        `${BASE_URL}${REQUEST_REJECT_API}/${connectionId}`,
+        {},
+        { withCredentials: true }
+      )
+      .then((res) => {
+        const response = res?.data;
+        const { success = false, data = null, message } = response;
+        if (success && data) {
+          fetchConnectionRequests();
+          dispatch(showSuccessMessage(message));
+        }
+      })
+      .catch((err) => {
+        const response = err?.response?.data;
+        const { success, message } = response;
+        if (!success) {
+          const displayMsg = message || "Failed to reject Connection request.";
+          dispatch(showErrorMessage(displayMsg));
+        }
+      });
+  };
 
   return (
     <div className="w-full flex justify-center text-gray-700">
@@ -11,43 +89,65 @@ const Requests = () => {
         </div>
         <div className="border-t border-gray-800"></div>
 
-        {connectionRequests &&
-          connectionRequests.map((record) => {
-            const { fromUserId } = record;
-            const { _id, firstName, lastName, photoUrl, age, gender, about } =
-              fromUserId;
+        {connectionRequests && connectionRequests.length !== 0 ? (
+          <>
+            {connectionRequests.map((record) => {
+              const { _id: connectionId, fromUserId } = record;
+              const {
+                _id: userId,
+                firstName,
+                lastName,
+                photoUrl,
+                age,
+                gender,
+                about,
+              } = fromUserId;
 
-            const fullName = `${firstName} ${lastName}`;
+              const fullName = `${firstName} ${lastName}`;
 
-            return (
-              <div key={_id} className="p-5 flex flex-col gap-4">
-                <div className="w-full bg-base-100 flex h-24 rounded-xl p-4 gap-5">
-                  <div className="avatar">
-                    <div className="w-16 rounded-xl">
-                      <img src={photoUrl} />
+              return (
+                <div key={userId} className="p-5 flex flex-col gap-4">
+                  <div className="w-full bg-base-100 flex h-24 rounded-xl p-4 gap-5">
+                    <div className="avatar">
+                      <div className="w-16 rounded-xl">
+                        <img src={photoUrl} />
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="flex flex-col justify-center w-full">
-                    <div className="font-bold">{fullName}</div>
-                    {gender && age && (
-                      <div className="text-sm opacity-60">{`${gender}, ${age}`}</div>
-                    )}
-                    <div className="text-sm opacity-70 line-clamp-1">
-                      {about}
+                    <div className="flex flex-col justify-center w-full">
+                      <div className="font-bold">{fullName}</div>
+                      {gender && age && (
+                        <div className="text-sm opacity-60">{`${gender}, ${age}`}</div>
+                      )}
+                      <div className="text-sm opacity-70 line-clamp-1">
+                        {about}
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="flex items-center justify-center gap-2">
-                    <button className="btn btn-sm btn-ghost">{`Reject`}</button>
-                    <button className="btn btn-sm btn-outline btn-primary">
-                      {`Accept`}
-                    </button>
+                    <div className="flex items-center justify-center gap-2">
+                      <button
+                        className="btn btn-sm btn-ghost"
+                        onClick={() => handleRequestReject(connectionId)}
+                      >
+                        {`Reject`}
+                      </button>
+                      <button
+                        className="btn btn-sm btn-outline btn-primary"
+                        onClick={() => handleRequestAccept(connectionId)}
+                      >
+                        {`Accept`}
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </>
+        ) : (
+          <div className="p-5 flex justify-center">
+            <h2>{`No request found.`}</h2>
+          </div>
+        )}
       </div>
     </div>
   );
